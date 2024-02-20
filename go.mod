ba46d727e38f81c6c8657d489a61d687491ded33
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-02-20 12:02:44 +0530

    created a helloworld server

--- go.mod
+++ go.mod
@@ -1,6 +1,11 @@
-module github.com/jaismanish15/Demo-go
+module Demo-go
 
 go 1.21.6
+
+require (
+	google.golang.org/grpc v1.61.1
+	google.golang.org/protobuf v1.32.0
+)
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -8,6 +13,4 @@ require (
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240213162025-012b6fc9bca9 // indirect
-	google.golang.org/grpc v1.61.1 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
 )
---
+++ greeter_client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "Demo-go/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Greeting)
+}
